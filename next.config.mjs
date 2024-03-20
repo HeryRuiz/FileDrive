d61b32e039e7abe9d98695520b5a58ea2687f0f8
hery Ruiz <heryrdev@gmail.com>
2024-03-20 19:03:04 -0400

    logic and css complete except for smaller devices

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "descriptive-starling-943.convex.cloud",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
