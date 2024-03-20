6054acdbdacfdeb51b0666b70848a1de0c5fd753
hery Ruiz <heryrdev@gmail.com>
2024-03-19 21:15:44 -0400

    testing out clerk components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-
+"use client";
+import { SignInButton, SignOutButton, SignedIn, useSession } from "@clerk/nextjs";
 
 export default function Home() {
+  const session = useSession();
   return (
-    <></>
+    <>
+      <SignedIn>
+        <div>You are signed in</div>
+        <SignOutButton mode="modal" />
+      </SignedIn>
+      <div>Always visible</div>
+      <SignInButton mode="modal" />
+    </>
   );
 }
