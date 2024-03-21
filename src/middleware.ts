a9c565462445f439546f788b1aec24d5d9034f5d
hery Ruiz <heryrdev@gmail.com>
2024-03-21 14:10:07 -0400

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
 import createMiddleware from "next-intl/middleware";
 
+const intlMiddleware = createMiddleware({
+  locales: ["en", "zh"],
+  defaultLocale: "en",
+});
 
 export default authMiddleware({
- 
+  beforeAuth: (req) => {
+    return intlMiddleware(req);
+  },
 
-  publicRoutes: ["/", "/en"],
+  publicRoutes: ["/"],
 });
 
 export const config = {
