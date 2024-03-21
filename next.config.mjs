1d32e39d8bf47bdfb587de5858355c2bac04f620
hery Ruiz <heryrdev@gmail.com>
2024-03-20 23:22:15 -0400

    d

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    hostname: "descriptive-starling-943.convex.cloud",
+    remotePatterns: [
+      {
+        hostname: "descriptive-starling-943.convex.cloud"
+      },
+    ],
   },
-  swcMinify: false,
 };
 
 export default nextConfig;
