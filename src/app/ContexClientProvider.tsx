9a395edcf169fb726bfd5896e1b3f4c2ef5747d6
hery Ruiz <heryrdev@gmail.com>
2024-03-20 22:52:53 -0400

    /ad

--- src/app/ContexClientProvider.tsx
+++ src/app/ContexClientProvider.tsx
@@ -5,7 +5,7 @@ import { ConvexReactClient } from "convex/react";
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
-const convex = new ConvexReactClient('https://descriptive-starling-943.convex.cloud');
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
   children,
