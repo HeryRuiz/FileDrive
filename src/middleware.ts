4d1fb3618c9be45e02a117549dca075021b3b805
hery Ruiz <heryrdev@gmail.com>
2024-03-21 00:26:04 -0400

    changed middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,19 @@
 import { authMiddleware } from "@clerk/nextjs";
+import createMiddleware from "next-intl/middleware";
+
+const intlMiddleware = createMiddleware({
+  locales: ["en", "zh"],
+  defaultLocale: "en",
+});
 
 export default authMiddleware({
+  beforeAuth: (req) => {
+    return intlMiddleware(req);
+  },
+
   publicRoutes: ["/"],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
