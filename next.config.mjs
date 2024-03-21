5260e73606f5311cd6477d2920a94a9de217d726
hery Ruiz <heryrdev@gmail.com>
2024-03-20 23:18:38 -0400

    try

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   images: {
     hostname: "descriptive-starling-943.convex.cloud",
-    domains: ["www.gravatar.com"],
-    formats: ["image/avif", "image/webp"],
   },
   swcMinify: false,
 };
